352eef84c5062306157be5c10dc13a4d9c4814d2
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-10-09 17:11:21 +0200

    Setup Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello 3")
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"]
+}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(8001, () => {
+    console.log('listening to port 8001')
+})
