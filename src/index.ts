a295e20723af96348ef081805bab2c2d92136b20
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-10-10 20:42:00 +0200

    TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
-import { routes } from './routes';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { routes } from "./routes";
+import { createConnection } from "typeorm";
 
-const app = express();
+createConnection()
+  .then(async (connection) => {
+    const app = express();
 
-app.use(express.json());
-app.use(cors({
-    origin: ["http://localhost:3000"]
-}));
+    app.use(express.json());
+    app.use(
+      cors({
+        origin: ["http://localhost:3000"],
+      })
+    );
 
-routes(app);
+    routes(app);
 
-app.listen(8000, () => {
-    console.log('listening to port 8000')
-})
+    app.listen(8000, () => {
+      console.log("listening to port 8000");
+    });
+  })
+  .catch((error) => console.log("Data Access Error: ", error));
