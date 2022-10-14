89d954a2743d1f194e8867e4ee17d68d736961a1
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-10-14 11:58:57 +0200

    Update user infor and password

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getManager} from "typeorm";
+import { getManager } from "typeorm";
 import { RegisterValidation } from "../validation/register.validation";
 import { User } from "../entity/user.entity";
 import bcyptjs from "bcryptjs";
@@ -68,9 +68,9 @@ export const Login = async (req: Request, res: Response) => {
 };
 
 export const AuthenticatedUser = async (req: Request, res: Response) => {
-    const {password, ...user}= req.user;
+  const { password, ...user } = req.user;
 
-    res.send(user);
+  res.send(user);
 };
 
 export const Logout = async (req: Request, res: Response) => {
@@ -79,4 +79,35 @@ export const Logout = async (req: Request, res: Response) => {
   res.send({
     message: "success",
   });
+};
+
+export const UpdateInfo = async (req: Request, res: Response) => {
+  const user = req.user;
+
+  const repository = getManager().getRepository(User);
+
+  await repository.update(user.id, req.body);
+
+  const {password, ...data} = await repository.findOneBy(user.id) ?? {};
+
+  res.send(data);
+};
+
+export const UpdatePassword = async (req: Request, res: Response) => {
+  const user = req.user;
+
+  if (req.body.password !== req.body.password_confirm) {
+    return res.status(400).send({
+      message: "Passwords do not match",
+    });
+  }
+
+  const repository = getManager().getRepository(User);
+  await repository.update(user.id, {
+    password: await bcyptjs.hash(req.body.password, 10)
+  });
+
+  const {password, ...data} = user;
+
+  res.send(data);
 };
