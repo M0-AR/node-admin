5b801ebeb62ef32161ddc298caae2ad640abc05e
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-10-19 12:11:03 +0200

    Permissions

--- src/entity/role.entity.ts
+++ src/entity/role.entity.ts
@@ -1,11 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Permission } from "./permission.entity";
 
 @Entity()
 export class Role {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Permission)
+  @JoinTable({
+    name: "role_permissions",
+    joinColumn: { name: "role_id", referencedColumnName: "id" },
+    inverseJoinColumn: { name: "permission_id", referencedColumnName: "id" },
+  })
+  permissions: Permission[];
 }
