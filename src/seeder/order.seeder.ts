51e8c6178084534a867bd9678d7cfb2b57cebc51
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-11-09 21:50:13 +0100

    Orders

---
+++ src/seeder/order.seeder.ts
@@ -0,0 +1,27 @@
+import { createConnection, getManager } from "typeorm";
+import { faker } from "@faker-js/faker";
+import { Order } from "../entity/order.entity";
+import { OrderItem } from "../entity/order-item.entity";
+
+createConnection().then(async (connection) => {
+  const orderRepository = getManager().getRepository(Order);
+  const orderItemRepository = getManager().getRepository(OrderItem);
+
+  for (let i = 0; i < 30; i++) {
+    const order = await orderRepository.save({
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+    });
+
+    const random = Math.floor(1 + Math.random() * (5 - 1 + 1)); // 10-100
+    for (let j = 0; j < random; j++) {
+      await orderItemRepository.save({
+        order,
+        product_title: faker.lorem.words(2),
+        price: Math.floor(10 + Math.random() * (100 - 10 + 1)), // 10-100
+        quantity: Math.floor(1 + Math.random() * (5 - 1 + 1)), // 1-5
+      });
+    }
+  }
+});
